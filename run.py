75e54215149e9bfa6dd143a63f4c4d0d24eac68d
KatVolkova <volkovakatrin87@yahoo.com>
2024-05-18 15:49:24 +0000

    Add update_balance_sheet function to receive users input and update google sheets

--- run.py
+++ run.py
@@ -13,7 +13,7 @@ GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('financial_statements')
 
 profit_and_loss_sheet = SHEET.worksheet('profit_and_loss')
-
+balance_sheet = SHEET.worksheet('balance_sheet')
 
 
 def update_profit_and_loss():
@@ -32,11 +32,21 @@ def update_profit_and_loss():
     interest_expenses = int(input("Please enter the number for interest expenses.The number should be between 1,000 and 10,000. "))
     profit_and_loss_sheet.update_acell('B25',interest_expenses)
 
-    
+def update_balance_sheet():
+    """Update the Balance Sheet numbers for particular lines in Google sheets balance_sheet tab"""  
+    print("The following should be updated in BS:\n\tCash and Cash Equivalents\n\tShort-Term Loans")
+
+    cash_and_equivalents = int(input("Please enter the number for cash. The number should be between 5,000 and 100,000. "))
+    balance_sheet.update_acell('B8',cash_and_equivalents)
+
+    short_term_loans = int(input("Please enter the number for short-term loans. The number should be between 1,000 and 50,000. "))
+    balance_sheet.update_acell('B20',short_term_loans)    
 
 def main():
     update_profit_and_loss()
-    print("The profit and loss account has been updated")
+    print("The Profit and loss account has been updated")
+    update_balance_sheet()
+    print("The Balance sheet has been updated")
 
 if __name__ == "__main__":
     main()
