7e36ff0191641841cbc8f5a643cc6e9ad8b83120
KatVolkova <volkovakatrin87@yahoo.com>
2024-05-18 15:38:35 +0000

    Add update_profit_and_loss function to recieve users input and update google sheets

--- run.py
+++ run.py
@@ -12,5 +12,31 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('financial_statements')
 
-data = SHEET.worksheet('profit_and_loss').get_all_values()
-print(data)
+profit_and_loss_sheet = SHEET.worksheet('profit_and_loss')
+
+
+
+def update_profit_and_loss():
+    """Update the profit and loss account numbers for particular lines in Google sheets profit_and_loss tab"""
+    print("Step 1. Update Profit and Loss account numbers:")
+
+    sales_revenue = int(input("Please enter the number for sales revenue. The number should be between 50,000 and 500,000. "))
+    profit_and_loss_sheet.update_acell('B5',sales_revenue)
+
+    purchased_inventory = int(input("Please enter the number for inventory. The number should be between 10,000 and 200,000. "))
+    profit_and_loss_sheet.update_acell('B9',purchased_inventory)
+
+    rent = int(input("Please enter the number for rent. The number should be between 5,000 and 20,000. "))
+    profit_and_loss_sheet.update_acell('B18',rent)
+
+    interest_expenses = int(input("Please enter the number for interest expenses.The number should be between 1,000 and 10,000. "))
+    profit_and_loss_sheet.update_acell('B25',interest_expenses)
+
+    
+
+def main():
+    update_profit_and_loss()
+    print("The profit and loss account has been updated")
+
+if __name__ == "__main__":
+    main()
